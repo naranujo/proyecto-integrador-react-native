9dd745aaf644144e1d3a552c14146b3900ea6d0c
Nicolás Araujo <naraujo@192.168.1.2>
2022-05-25 20:19:41 -0300

    inicializo el proyecto

---
+++ pi-react-native/App.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
